42db8727b9c2edd0755f1af7d5b22c8761fd890b
Wizbisy <155181901+Wizbisy@users.noreply.github.com>
2025-09-29 11:12:29 +0100

    Update web3Features.ts

--- client/src/hooks/web3Features.ts
+++ client/src/hooks/web3Features.ts
@@ -1,11 +1,1 @@
-import {
-    base,
-} from "@reown/appkit/networks";
 
-const knownChains = {
-    [base.id]: base,
-};
-
-export function getNetworkInfo(chainId: number | string) {
-    return knownChains[chainId as keyof typeof knownChains] || null;
-}
